6f39fed13dd09a88b93b60457ef48a2e71c5286d
martinbarker <martinbarker99@gmail.com>
2020-01-20 23:37:03 -0800

    removed comments

--- commandLineMusicVideo/__init__.py
+++ commandLineMusicVideo/__init__.py
@@ -28,8 +28,6 @@ def fullAlbum(songsFilepath, audioFormat, imageFilepath, outputResolution):
                 songLocation = songsFilepath + '/' + filename
                 concatString = concatString + songLocation + '|'
 
-        #create concatAudio string     "concat:fighter.mp3|03life.mp3"
-        #render concatAudio.mp3    ffmpeg -i "concat:fighter.mp3|03life.mp3" -acodec copy concatAudio.mp3
         print('concatString = ', concatString)
         os.system('ffmpeg -i "' + concatString + '" -acodec copy "' + songsFilepath + '/concatAudio.mp3"')
 
@@ -37,8 +35,6 @@ def fullAlbum(songsFilepath, audioFormat, imageFilepath, outputResolution):
         os.system('rm "'+ songsFilepath + '/concatAudio.mp3"')
 
     elif audioFormat == 'flac':
-        #os.system('rm inputs.txt')
-        #os.system('rm "'+ songsFilepath + '/concatAudio.mp3"')
         os.system('touch songinputs.txt')
         arr = os.listdir(songsFilepath)
         for filename in arr:
@@ -49,7 +45,6 @@ def fullAlbum(songsFilepath, audioFormat, imageFilepath, outputResolution):
                     myfile.write("file '" + songLocationString +"' \n")
 
         os.system("ffmpeg -f concat -safe 0 -i songinputs.txt -safe 0 '" + songsFilepath + "/concatAudio.mp3'")
-        #os.system('rm inputs.txt')
         renderVideo(songsFilepath+'/concatAudio.mp3', 'concatAudio.mp3', imageFilepath, outputResolution, songsFilepath+'/fullAlbum')
         os.system('rm "'+ songsFilepath + '/concatAudio.mp3"')
 
@@ -79,23 +74,17 @@ def outputFilenameParse(outputFilename):
     return outputFilename
 
 def renderEachSong(songsFilepath, imageFilepath, audioFormat, outputResolution):
-    #renderEachSong(songsFilepath, imageFilepath, audioFormat, outputResolution)
-        #for each song
+    #for each file in filepath
     arr = os.listdir(songsFilepath)
     print(arr)
     for filename in arr:
         if filename.endswith(audioFormat):
             outputFilename = filename[:-len(audioFormat)-1]
-
             outputFilename = outputFilenameParse(outputFilename)
-
             print('outputFilename = [', outputFilename, ']')
             outputFilename = songsFilepath + '/' + outputFilename            
             print('outputFilename = [', outputFilename, ']')
-
             renderVideo(songsFilepath+''+filename, filename, imageFilepath, outputResolution, outputFilename)
-
-    
     print('')
     
 
@@ -151,8 +140,8 @@ if '-songs' in sys.argv:
     if '-fullAlbumOnly' in sys.argv:
         fullAlbum(songsFilepath, audioFormat, imageFilepath, outputResolution)
 
-    elif '-fullAlbum' in sys.argv: # and sys.argv.index('-fullalbum') < sys.argv.index('-songs'):
-        print('fullAlbum') #comesfirst')
+    elif '-fullAlbum' in sys.argv:
+        print('fullAlbum')
         renderEachSong(songsFilepath, imageFilepath, audioFormat, outputResolution)
         fullAlbum(songsFilepath, audioFormat, imageFilepath, outputResolution)
     else:
